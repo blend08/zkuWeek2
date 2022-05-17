898f84c288a149d3544ae6cc97022510e9b763bc
blend08 <blenderrrr08@gmail.com>
2022-05-17 19:20:38 +0800

    Submission

--- Part1/contracts/MerkleTree.sol
+++ Part1/contracts/MerkleTree.sol
@@ -11,10 +11,40 @@ contract MerkleTree is Verifier {
 
     constructor() {
         // [assignment] initialize a Merkle tree of 8 with blank leaves
+        for (uint32 i = 1; i < 16; i++){
+            hashes.push(0);
+        }
+        
     }
 
     function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
         // [assignment] insert a hashed leaf into the Merkle tree
+        
+        uint256 _nextIndex = index;
+        require(_nextIndex != uint32(2)**3, "Merkle tree is full. No more leaves can be added");
+        uint256 currentIndex = _nextIndex;
+        uint256 currentLevelHash = hashedLeaf;
+        uint256 left;
+        uint256 right;
+
+        for (uint32 i = 1; i < 3; i++) {
+        if (currentIndex % 2 == 0) {
+            left = currentLevelHash;
+            right = 0;
+            hashes[i] = currentLevelHash;
+        } else {
+            left = hashes[i];
+            right = currentLevelHash;
+        }
+        currentLevelHash = PoseidonT3.poseidon([left, right]);
+        currentIndex /= 2;
+        }
+
+        
+        root = currentLevelHash;
+        index += 1;
+        return _nextIndex;
+
     }
 
     function verify(
@@ -25,5 +55,7 @@ contract MerkleTree is Verifier {
         ) public view returns (bool) {
 
         // [assignment] verify an inclusion proof and check that the proof root matches current root
+
+        return verifyProof(a, b, c, input);
     }
 }
